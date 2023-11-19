8aae332e9f96bfd00f5428d0ac8f6f6367c399f6
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-19 15:56:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+
+	let ss1=s1.toLowerCase();
+	let ss2=s2.toLowerCase();
+	
+	let n1=s1.length;
+	let n2=s2.length;
+
+	for(let i=0;i<n1;i++)
+		{
+			let j=i+n2;
+			let s=ss1.substring(i,j);
+  
+			if(s==ss2) 
+			{
+				return i;
+			}
+		}
+
+	return -1;
 }
 
 // Please do not change the code below
